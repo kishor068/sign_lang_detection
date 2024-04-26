ffa4e27400b38014e02b2a246d0d5bced6a0ab4a
Manaswis <119678606+Manaswis@users.noreply.github.com>
2024-04-26 09:30:31 +0530

    <signlang detection>

---
+++ tempCodeRunnerFile.py
@@ -0,0 +1,46 @@
+if interrupt & 0xFF == ord('d'):
+    #     cv2.imwrite(directory+'D/'+str(count['d'])+'.png',frame)
+    # if interrupt & 0xFF == ord('e'):
+    #     cv2.imwrite(directory+'E/'+str(count['e'])+'.png',frame)
+    # if interrupt & 0xFF == ord('f'):
+    #     cv2.imwrite(directory+'F/'+str(count['f'])+'.png',frame)
+    # if interrupt & 0xFF == ord('g'):
+    #     cv2.imwrite(directory+'G/'+str(count['g'])+'.png',frame)
+    # if interrupt & 0xFF == ord('h'):
+    #     cv2.imwrite(directory+'H/'+str(count['h'])+'.png',frame)
+    # if interrupt & 0xFF == ord('i'):
+    #     cv2.imwrite(directory+'I/'+str(count['i'])+'.png',frame)
+    # if interrupt & 0xFF == ord('j'):
+    #     cv2.imwrite(directory+'J/'+str(count['j'])+'.png',frame)
+    # if interrupt & 0xFF == ord('k'):
+    #     cv2.imwrite(directory+'K/'+str(count['k'])+'.png',frame)
+    # if interrupt & 0xFF == ord('l'):
+    #     cv2.imwrite(directory+'L/'+str(count['l'])+'.png',frame)
+    # if interrupt & 0xFF == ord('m'):
+    #     cv2.imwrite(directory+'M/'+str(count['m'])+'.png',frame)
+    # if interrupt & 0xFF == ord('n'):
+    #     cv2.imwrite(directory+'N/'+str(count['n'])+'.png',frame)
+    # if interrupt & 0xFF == ord('o'):
+    #     cv2.imwrite(directory+'O/'+str(count['o'])+'.png',frame)
+    # if interrupt & 0xFF == ord('p'):
+    #     cv2.imwrite(directory+'P/'+str(count['p'])+'.png',frame)
+    # if interrupt & 0xFF == ord('q'):
+    #     cv2.imwrite(directory+'Q/'+str(count['q'])+'.png',frame)
+    # if interrupt & 0xFF == ord('r'):
+    #     cv2.imwrite(directory+'R/'+str(count['r'])+'.png',frame)
+    # if interrupt & 0xFF == ord('s'):
+    #     cv2.imwrite(directory+'S/'+str(count['s'])+'.png',frame)
+    # if interrupt & 0xFF == ord('t'):
+    #     cv2.imwrite(directory+'T/'+str(count['t'])+'.png',frame)
+    # if interrupt & 0xFF == ord('u'):
+    #     cv2.imwrite(directory+'U/'+str(count['u'])+'.png',frame)
+    # if interrupt & 0xFF == ord('v'):
+    #     cv2.imwrite(directory+'V/'+str(count['v'])+'.png',frame)
+    # if interrupt & 0xFF == ord('w'):
+    #     cv2.imwrite(directory+'W/'+str(count['w'])+'.png',frame)
+    # if interrupt & 0xFF == ord('x'):
+    #     cv2.imwrite(directory+'X/'+str(count['x'])+'.png',frame)
+    # if interrupt & 0xFF == ord('y'):
+    #     cv2.imwrite(directory+'Y/'+str(count['y'])+'.png',frame)
+    # if interrupt & 0xFF == ord('z'):
+    #     cv2.imwrite(directory+'Z/'+str(count['z'])+'.png',frame)
